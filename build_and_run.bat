753bae9e0b1dfe64f862dc77263f4e9ed266c757
LenoYT <135437124+LenoYT@users.noreply.github.com>
2024-09-18 11:54:20 +0200

    Update build_and_run.bat

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,36 +1,29 @@
 @echo off
-REM Ustawienie zmiennych
 set SRC_DIR=.
 set BUILD_DIR=%SRC_DIR%\build
 set INCLUDE_DIR=%SRC_DIR%
 
-REM Utwórz katalog build, jeśli nie istnieje
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-REM Nazwy plików
 set SRC_FILE=gameR.cpp
 set OBJ_FILE=gameR.o
 set LIB_FILE=libgameR.a
 set MAIN_FILE=main.cpp
 set EXE_FILE=program.exe
 
-REM Kompilacja pliku źródłowego do pliku obiektowego
-echo Kompilowanie %SRC_FILE% do %OBJ_FILE%
+echo C: %SRC_FILE% :: %OBJ_FILE%
 g++ -c %SRC_DIR%\%SRC_FILE% -o %BUILD_DIR%\%OBJ_FILE% -I%INCLUDE_DIR%
 
-REM Tworzenie biblioteki statycznej z pliku obiektowego
-echo Tworzenie %LIB_FILE% z %OBJ_FILE%
+echo Mak %LIB_FILE% : %OBJ_FILE%
 ar rcs %BUILD_DIR%\%LIB_FILE% %BUILD_DIR%\%OBJ_FILE%
 
-REM Kompilacja pliku main.cpp, linkowanie z biblioteką
-echo Kompilowanie %MAIN_FILE% do %EXE_FILE%
+echo C: %MAIN_FILE% :: %EXE_FILE%
 g++ %SRC_DIR%\%MAIN_FILE% -o %BUILD_DIR%\%EXE_FILE% -L%BUILD_DIR% -lgameR -I%INCLUDE_DIR%
 
-REM Uruchamianie programu
-echo Uruchamianie %EXE_FILE%
+echo R: %EXE_FILE% in new cmd proccess
 cd /d "%BUILD_DIR%"
 start cmd /k "%EXE_FILE%"
 
-REM Kończenie
-echo Proces zakończony
+REM End
 pause
+echo Ended
